8fc23543f9c481c1bb8005a794c7b31bbc58ad95
lazaris <lazaris@tak.wi.mit.edu>
2022-01-19 17:35:45 -0500

    Corrected file for extracting and extending TSS

---
+++ find_tss_and_extend.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#This is a script that extract the TSSs 
+
+input=$1
+extend=$2
+
+extend_kb=$(echo $extend / 1000 | bc)
+
+awk -F "\t" '{if ($6 == "+") {print  $1"\t"$2"\t"$2+1"\t"$4"\t"$5"\t"$6} else {print  $1"\t"$3-1"\t"$3"\t"$4"\t"$5"\t"$6}  }' $input > ${input%.bed}.tss.bed
+
+# Get name for the output file
+gene_tss=${input%.bed}.tss.bed
+
+extended_gene_tss=${gene_tss%.bed}.plus_minus_${extend_kb}kb.bed
+# Get a certain window around the TSS
+slopBed -i $gene_tss -g hg19.chrom.sizes -l $extend -r $extend -s  > $extended_gene_tss 
