1f98578114ec0c93c0f8b2841d5c0cfbb3a68db4
lazaris <lazaris@tak.wi.mit.edu>
2022-01-26 17:55:48 -0500

    Added promoter exclusion

--- extract_enhancers_from_table.sh
+++ extract_enhancers_from_table.sh
@@ -8,20 +8,25 @@
 # c. super-enhancers (*SuperEnhancers.bed)
 
 # Check for input
-if [ $# -ne 1 ]; then
+if [ $# -ne 2 ]; then
 	echo "Please, provide the correct number of arguments..."
-	echo "USAGE: extract_enhancers_from_table.sh [INPUT; *AllEnhancers.table.txt]"
+	echo "USAGE: extract_enhancers_from_table.sh [INPUT; *AllEnhancers.table.txt] [INPUT; promoters.bed]"
 	exit 1
 fi
 
 input=$1
+promoters=$2
 
 all_enhancer=${input%.table.txt}.bed
 
 # Get all enhancers in a bed file
 cat $input | grep -v '#' | sed 1d | \
 	awk '{print $2"\t"$3"\t"$4"\t"$1"\t""0""\t""."}' | \
-	sort | uniq | sortBed > $all_enhancer
+	sort | uniq | sortBed > $all_enhancer.temp
+
+# Now exclude the promoters to get the actual enhancers
+bedtools intersect -v -wa -a $all_enhancer.temp -b promoters.bed > $all_enhancer
+rm -rf $all_enhancer.temp
 
 # Get the number of all enhancers
 echo "All enhancers: $(wc -l $all_enhancer | cut -d' ' -f1)"
@@ -31,7 +36,11 @@ typical_enhancer=$(echo $all_enhancer | sed 's/All/Typical/')
 # Get the typical enhancers in a bed file
 cat $input | grep -v '#' | sed 1d | \
 	awk '{ if ($10==0) print $2"\t"$3"\t"$4"\t"$1"\t""0""\t""."}' | \
-        sort | uniq | sortBed > $typical_enhancer
+        sort | uniq | sortBed > $typical_enhancer.temp
+
+# Now exclude the promoters to get the actual typical enhancers
+bedtools intersect -v -wa -a $typical_enhancer.temp -b promoters.bed > $typical_enhancer
+rm -rf $typical_enhancer.temp
 
 # Get the number of typical enhancers
 echo "Typical enhancers: $(wc -l $typical_enhancer | cut -d' ' -f1)"
@@ -41,7 +50,11 @@ super_enhancer=$(echo $all_enhancer | sed 's/All/Super/')
 # Get the superenhancers in a bed file
 cat $input | grep -v '#' | sed 1d | \
         awk '{ if ($10==1) print $2"\t"$3"\t"$4"\t"$1"\t""0""\t""."}' | \
-        sort | uniq | sortBed > $super_enhancer
+        sort | uniq | sortBed > $super_enhancer.temp
 
-# Get the number of typical enhancers
+# Now exclude the promoters to get the actual super-enhancers
+bedtools intersect -v -wa -a $super_enhancer.temp -b promoters.bed > $super_enhancer
+rm -rf $super_enhancer.temp
+
+# Get the number of super-enhancers
 echo "Super-enhancers: $(wc -l $super_enhancer | cut -d' ' -f1)"
